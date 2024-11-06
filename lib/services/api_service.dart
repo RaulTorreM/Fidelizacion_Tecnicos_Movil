a6e437e0cc52fea928814a035b711a1d0bc5a5c6
RaúlTM <77331712@continental.edu.pe>
2024-11-06 17:46:37 -0500

    cambio de ip

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -9,9 +9,9 @@ import '../data/models/csrf_response.dart';
 import '../data/models/recompensa.dart';
 import '../data/models/tecnico_response.dart';
 
-part 'api_service.g.dart'; // Asegúrate de tener el archivo generado
+part 'api_service.g.dart'; 
 
-@RestApi(baseUrl: "http://192.168.0.15/FidelizacionTecnicos/public/api/")
+@RestApi(baseUrl: "http://192.168.137.54/FidelizacionTecnicos/public/api/")
 abstract class ApiService {
   factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
 
--- lib/services/api_service.g.dart
+++ lib/services/api_service.g.dart
@@ -13,7 +13,7 @@ class _ApiService implements ApiService {
     this._dio, {
     this.baseUrl,
   }) {
-    baseUrl ??= 'http://192.168.0.15/FidelizacionTecnicos/public/api/';
+    baseUrl ??= 'http://192.168.137.54/FidelizacionTecnicos/public/api/';
   }
 
   final Dio _dio;
